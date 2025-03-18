973b84977957dbc93bc98590149906df40963661
Mihir-Raj-Singh <mihirrajsingh123@gmail.com>
2025-03-18 22:24:59 +0530

    General Update

--- lua/plugins/code_runner.lua
+++ lua/plugins/code_runner.lua
@@ -1,20 +1,20 @@
 return {
   "CRAG666/code_runner.nvim",
   config = function()
-    -- Set up the code_runner plugin
     require("code_runner").setup({
       mode = "float",
       filetype = {
         python = "python3 -u",
         c = {
-          'cd "$dir" && gcc "$fileName" -o /tmp/"$fileNameWithoutExt" && /tmp/"$fileNameWithoutExt" && rm /tmp/"$fileNameWithoutExt"',
+          'cd "$dir" && gcc "$fileName" -o /tmp/"$fileNameWithoutExt" && /tmp/"$fileNameWithoutExt"; [ -f /tmp/"$fileNameWithoutExt" ] && rm /tmp/"$fileNameWithoutExt"',
         },
         cpp = {
-          'cd "$dir" && g++ "$fileName" -o /tmp/"$fileNameWithoutExt" && /tmp/"$fileNameWithoutExt" && rm /tmp/"$fileNameWithoutExt"',
+          'cd "$dir" && g++ "$fileName" -o /tmp/"$fileNameWithoutExt" && /tmp/"$fileNameWithoutExt"; [ -f /tmp/"$fileNameWithoutExt" ] && rm /tmp/"$fileNameWithoutExt"',
         },
+        js = "node $fileName",
       },
       float = {
-        border = "rounded", -- Rounded border style
+        border = "rounded",
       },
     })
   end,
